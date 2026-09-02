60f8564794d3f8fe55942387402074b7d417ef0c
28sbardelli <28sbardelli@live.ytech.edu>
2026-09-02 08:21:58 -0400

    Finished required testing to confirm game functionality

--- docpac_030309/Button Masher Game/BardelliSamuel/button-masher-game/game.js
+++ docpac_030309/Button Masher Game/BardelliSamuel/button-masher-game/game.js
@@ -2,11 +2,9 @@ let indx = 0
 let prevFA = false
 let Apressed = false
 let Startpressed = false
-let prevFS = false
+let prevFS = true
 let ASC = false
-let SSC = false
 let gameEnded = false
-
 let score = 0
 let timer = 20
 let direction
@@ -24,10 +22,14 @@ let timerID = setInterval(function () {
     }
     if (timer == 0) {
         gameEnded = true
-        timerBox.textContent = "Game Over!"
+        timerBox.textContent = "Game Over! Press start to restart."
         clearInterval(timerID)
     }
 }, 1000)
+
+direction = Math.floor(Math.random() * 4)
+directionBox.textContent = dirMap[direction]
+
 let directionID = setInterval(function () {
     if (gameEnded == false) {
         direction = Math.floor(Math.random() * 4)
@@ -45,6 +47,7 @@ function pollGamepad() {
         Apressed = true
         if (prevFA == false) {
             ASC = true
+            console.log("a pressed")
         }
         else{
             ASC = false
@@ -57,22 +60,51 @@ function pollGamepad() {
     if (gamepad.buttons[9].pressed) {
         Startpressed = true
         if (prevFS == false) {
-            SSC = true
-        }
-        else{
-            SSC = false
+            location.reload()
         }
     }
     else {
         Startpressed = false
     }
 
-    console.log(gamepad.axes[1])
+    function dirMatch(reqDirection, horAX, verAX){
+    let helddirection
+        if (verAX > 0.5){
+        helddirection = dirMap[2]
+    }
+    else if (verAX < -0.5){
+        helddirection = dirMap[0]
+    }
+    else if (horAX > 0.5){
+        helddirection = dirMap[1]
+    }
+    else if (horAX < -0.5){
+        helddirection = dirMap[3]
+    }
+    else{
+        helddirection = null
+    }
+    console.log(helddirection)
+
+    if (reqDirection == helddirection){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+    if (ASC == true && timer > 0 && gameEnded == false && dirMatch(dirMap[direction], gamepad.axes[0], gamepad.axes[1]) == true){
+        score ++
+        scoreBox.textContent = `Score: ${score}`
+    }
 
     prevFA = Apressed
     prevFS = Startpressed
     requestAnimationFrame(pollGamepad)
 }
+
+
 
 window.addEventListener("gamepadconnected", function (event) {
     indx = event.gamepad.index
