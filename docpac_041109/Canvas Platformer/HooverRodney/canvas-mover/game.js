8d0b8fcbbd67b38ed87da37b30cec2cf10808e7b
28rhoover <28rhoover@live.ytech.edu>
2026-09-11 13:48:30 -0400

    Updating to Canvas Mover First Stretch Goal
    Updating the canvas mover project to include the changes I've added to meet the requirements for the first stretch goal

--- docpac_041109/Canvas Platformer/HooverRodney/canvas-mover/game.js
+++ docpac_041109/Canvas Platformer/HooverRodney/canvas-mover/game.js
@@ -9,16 +9,61 @@ let player = {
     width: 20,
     height: 20,
     speed: 5,
+    vy: 0,
+    vx: 0
     //spawns the player in the center of the screen, accounting for the size of the player and the dimensions of the canvas
 }
+
+let isGrounded;
+let leftPressed = false;
+let rightPressed = false;
+
+const gravity = 0.15;
 
 function drawPlayer() {
     ctx.fillRect(player.x, player.y, player.width, player.height);
 }
 
+function checkBorders() {
+    if (player.y >= canvas.height - player.height) { //downcheck
+        player.y = 580;
+        player.vy = 0;
+    }
+    if (player.y <= 0) {//upcheck
+        player.y = 0;
+        player.vy = 0;
+    }
+    if (player.x <= 0) {//leftcheck
+        player.x = 0;
+        player.vx = 0;
+    }
+    if (player.x >= canvas.width - player.width) {//right check
+        player.x = 780;
+    }
+}
+
+function checkDirection() {
+    if (leftPressed == true && rightPressed == false) {
+        player.vx = -5;
+    } else if (leftPressed == false && rightPressed == true) {
+        player.vx = 5;
+    } else {
+        player.vx = 0;
+    }
+}
+
 function loop() {
+    player.vy += gravity;
+    player.y += player.vy;
+    player.x += player.vx;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //player updated here when movement is added
+    if (player.y <= canvas.height - player.height) {//when it's less than or equal 580
+        isGrounded = false;
+    } else if (player.y > canvas.height - player.height) {
+        isGrounded = true;
+    }
+    checkDirection();
+    checkBorders();
     drawPlayer();
     requestAnimationFrame(loop);
 }
@@ -26,29 +71,36 @@ function loop() {
 window.addEventListener("keydown", (e) => {
     switch (e.key) {
         case 'a':
-            player.x -= player.speed; //move left
-            if (player.x <= 0) {
-                player.x = 0;
-            }
+            leftPressed = true;
             break;
         case 's':
             player.y += player.speed; //move down
-            if (player.y >= canvas.height - player.height) {
-                player.y = 580;
-            }
             break;
         case 'w':
-            player.y -= player.speed; //move up
-            if (player.y <= 0) {
-                player.y = 0;
+            if (isGrounded == true) {
+                player.vy -= player.speed; //move up
+            }
+            break;
+        case ' ':
+            if (isGrounded == true) {
+                player.vy -= player.speed; //move up
             }
             break;
         case 'd':
-            player.x += player.speed; //move right
-            if (player.x >= canvas.width - player.width) {
-                player.x = 780;
-            }
+            rightPressed = true;
+            break;
     }
 });
 
-window.onload = loop();
+window.addEventListener("keyup", (e) => {
+    switch (e.key) {
+        case 'a':
+            leftPressed = false;
+            break;
+        case 'd':
+            rightPressed = false;
+            break;
+    }
+})
+
+window.onload = loop;
